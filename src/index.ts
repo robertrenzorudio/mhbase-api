9ff8b26d9785d7aa2c268c4896395d631e25418f
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-06-11 23:31:42 -0700

    Added express req, and res to context.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
+import { PrismaClient } from '@prisma/client';
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
-import { context } from './context/context';
 import buildSchema from './schema';
 
 (async () => {
+  const prismaOptions: any =
+    process.env.NODE_ENV === 'dev' ? { log: ['query'] } : {};
+  const prisma = new PrismaClient(prismaOptions);
+
   const app = express();
   const schema = await buildSchema();
 
   const apolloServer = new ApolloServer({
     schema: schema,
-    context: context,
+    context: ({ req, res }) => ({
+      req,
+      res,
+      prisma: prisma,
+    }),
   });
 
   apolloServer.applyMiddleware({
@@ -18,6 +26,10 @@ import buildSchema from './schema';
   });
 
   const port = process.env.PORT || 4000;
+  app.use('/', (req: express.Request, res: express.Response) => {
+    res.send('Hello World!');
+  });
+
   app.listen(port, () => {
     console.log(`🚀 Server ready at: http://localhost:${port}`);
   });
