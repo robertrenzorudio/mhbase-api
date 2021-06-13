28ab5382c611ee8c6ec02e9aaa6118a02eaca524
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-06-12 21:54:02 -0700

    - Items now uses the new ItemArgs with ItemSearchType field. - Items query now uses ErrorInterceptor middleware

--- src/schema/items/item.resolver.ts
+++ src/schema/items/item.resolver.ts
@@ -8,11 +8,13 @@ import {
   Resolver,
   UseMiddleware,
 } from 'type-graphql';
-import { createPaginationOption } from '../../utils/createPaginationOption';
+import { createPaginationOptions } from '../../utils/createPaginationOptions';
 import { Context } from '../../types';
 import { ItemArgs } from './item.args';
 import { Item } from './item.model';
 import { RateLimit } from '../../middlewares/rateLimit';
+import { createItemWhereInput } from '../../utils/createWhereInput';
+import { ErrorInterceptor } from '../../middlewares/errorInterceptor';
 
 @Resolver()
 export class ItemResolver {
@@ -26,23 +28,23 @@ export class ItemResolver {
   }
 
   @Query(() => [Item])
-  @UseMiddleware(RateLimit())
-  async items(
-    @Args() args: ItemArgs,
-    @Ctx() ctx: Context
-  ): Promise<Item[] | null> {
-    // If cursor based: prioritize "after" if both is given.
+  @UseMiddleware(RateLimit(), ErrorInterceptor)
+  async items(@Args() args: ItemArgs, @Ctx() ctx: Context): Promise<Item[]> {
+    // If cursor based: prioritize "after" if skip is also given.
     // If both cursor and skip is given, use cursor.
-    const paginationOpt = createPaginationOption({
+    const paginationOpt = createPaginationOptions({
       take: args.take,
       skip: args.skip,
       before: args.before,
       after: args.after,
     });
 
+    const where = args.query
+      ? createItemWhereInput(args.query, args.itemSearchType)
+      : undefined;
     return ctx.prisma.item.findMany({
       ...paginationOpt,
-      where: { name: { contains: args.name } },
+      ...where,
     });
   }
 }
