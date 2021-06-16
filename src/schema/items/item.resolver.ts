6a57360946e8568ef17cb93373649a79ded3ac87
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-06-15 21:24:02 -0700

    ItemResolver: Refactored to extend base resolver. ItemArgs: Removed all fields except name.

--- src/schema/items/item.resolver.ts
+++ src/schema/items/item.resolver.ts
@@ -1,50 +1,16 @@
 import 'reflect-metadata';
-import {
-  Arg,
-  Args,
-  Ctx,
-  Int,
-  Query,
-  Resolver,
-  UseMiddleware,
-} from 'type-graphql';
-import { createPaginationOptions } from '../../utils/createPaginationOptions';
-import { Context } from '../../types';
+import { Resolver } from 'type-graphql';
 import { ItemArgs } from './item.args';
 import { Item } from './item.model';
-import { RateLimit, ErrorInterceptor } from '../../middlewares';
-import { createItemWhereInput } from '../../utils';
+import { createBaseResolver } from '../baseResolver';
+import { EntityName } from '../../enums';
 
-@Resolver()
-export class ItemResolver {
-  @Query(() => Item, { nullable: true })
-  @UseMiddleware(RateLimit())
-  async item(
-    @Arg('id', () => Int) id: number,
-    @Ctx() ctx: Context
-  ): Promise<Item | null> {
-    return ctx.prisma.item.findUnique({ where: { id: id } });
-  }
+const itemBaseResolver = createBaseResolver(
+  'item',
+  Item,
+  ItemArgs,
+  EntityName.Item
+);
 
-  @Query(() => [Item])
-  @UseMiddleware(RateLimit(), ErrorInterceptor)
-  async items(@Args() args: ItemArgs, @Ctx() ctx: Context): Promise<Item[]> {
-    // If cursor based: prioritize "after" if skip is also given.
-    // If both cursor and skip is given, use cursor.
-    const paginationOpt = createPaginationOptions({
-      take: args.take,
-      skip: args.skip,
-      before: args.before,
-      after: args.after,
-    });
-
-    const where = args.query
-      ? createItemWhereInput(args.query, args.itemSearchType)
-      : undefined;
-
-    return ctx.prisma.item.findMany({
-      ...paginationOpt,
-      where,
-    });
-  }
-}
+@Resolver(Item)
+export class ItemResolver extends itemBaseResolver {}
