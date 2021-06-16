32f9611b7f470022fc87c6ce1997ae3df5784f54
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-06-15 21:09:10 -0700

    Removed skip field and renamed take to limit.

--- src/schema/pagination/pagination.args.ts
+++ src/schema/pagination/pagination.args.ts
@@ -6,12 +6,7 @@ export class PaginationArgs {
   @Field(() => Int, { defaultValue: 25 })
   @Min(1)
   @Max(50)
-  take: number;
-
-  // Offset-based pagination.
-  @Field(() => Int, { nullable: true })
-  @Min(0)
-  skip?: number;
+  limit: number;
 
   // Cursor-based pagination
   @Field(() => Int, { nullable: true })
