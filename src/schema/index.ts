056e58eb20527522c559faecb1783cc9fc9d6031
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2021-07-01 00:11:53 -0700

    Schema is now built using buildSchema. Added redis in context.

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,18 +1,14 @@
 import 'reflect-metadata';
 import { GraphQLSchema } from 'graphql';
-import { makeExecutableSchema } from 'graphql-tools';
-import { buildTypeDefsAndResolvers } from 'type-graphql';
-import { resolvers as resolversArr } from './resolvers';
+import { buildSchema } from 'type-graphql';
+import { resolvers } from './resolvers';
 
-const buildSchema = async (): Promise<GraphQLSchema> => {
-  const { typeDefs, resolvers } = await buildTypeDefsAndResolvers({
-    resolvers: resolversArr,
-    emitSchemaFile: true,
+const getSchema = async (): Promise<GraphQLSchema> => {
+  const schema = await buildSchema({
+    resolvers,
   });
-
-  const schema = makeExecutableSchema({ typeDefs, resolvers });
 
   return schema;
 };
 
-export default buildSchema;
+export default getSchema;
